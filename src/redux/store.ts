a09ea6c0c53450754ba227934c7873590854e2a0
Mohd Shahjahan <md.shahjahan786pubg@gmail.com>
2025-04-23 04:55:26 +0530

    added email, sms and url qr  reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import dashboardReducer from "./features/dashboard/dashboardSlice"
+import urlReducer from "./features/url/urlSlice"
+import smsReducer from "./features/sms/smsSlice"
+import emailReducer from "./features/email/emailSlice"
 import { useDispatch } from 'react-redux'
 
 export const store = configureStore({
   reducer: {
-    dashboard :dashboardReducer
+    dashboard :dashboardReducer,
+    url:urlReducer,
+    sms:smsReducer,
+    email:emailReducer,
   },
 })
 
